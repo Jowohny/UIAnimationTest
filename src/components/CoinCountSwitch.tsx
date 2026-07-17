9dd32988b25908c629e255ab2522bfec876dd1f1
Jowohny <vujohny109238@gmail.com>
2026-07-16 17:35:35 -0700

    coin count switch
    to change the number of visible coins on the screen, works in tandem with the animation type switch

--- src/components/CoinCountSwitch.tsx
+++ src/components/CoinCountSwitch.tsx
@@ -2,16 +2,25 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
-export type SwitchType = 'center' | 'start' | 'end' | 'edges' | 'random'
-
-interface AnimationSwitchProps {
-  currentType: SwitchType;
-	onTypeChange: (selectedType: SwitchType) => void;
+export type CoinCount = {
+	name: string,
+	gap: number,
+	tile: number,
+	pad: number
 }
 
-const animationTypes: SwitchType[]  = ['center', 'start', 'end', 'edges', 'random']
+export interface CoinCountSwitchProps {
+  currentCount: CoinCount;
+	onTypeChange: (selectedType: CoinCount) => void;
+}
 
-export const AnimationSwitch = ({ currentType, onTypeChange }: AnimationSwitchProps ) => {
+export const animationTypes: CoinCount[]  = [
+	{ name: 'little', gap: 20, tile: 300, pad: 40 },
+	{ name: 'moderate', gap: 15, tile: 150, pad: 30 },
+	{ name: 'most', gap: 5, tile: 50, pad: 10 }
+]
+
+export const CoinCountSwitch = ({ currentCount, onTypeChange }: CoinCountSwitchProps ) => {
 	const container = useRef<HTMLDivElement>(null)
 
 	useGSAP(() => {
@@ -23,7 +32,8 @@ export const AnimationSwitch = ({ currentType, onTypeChange }: AnimationSwitchPr
 				opacity: 0,
 				duration: 1,
 				ease: 'sine.out',
-				y: -30
+				y: -30,
+				delay: 0.5
 			}).fromTo('.options', 
 				{ scale: 0 },
 				{ scale: 1, duration: 0.7, opacity: 1, stagger: 0.1, ease: 'none' },
@@ -36,12 +46,12 @@ export const AnimationSwitch = ({ currentType, onTypeChange }: AnimationSwitchPr
 		<div ref={container} className="px-4 py-2 bg-blue-800 flex border border-4 border-white flex-row gap-4 rounded-full">
 			{animationTypes.map(type => (
 				<div 
-					key={type} 
+					key={type.name} 
 					onClick={() => onTypeChange(type)}
 					className={`options opacity-0 text-white rounded-full font-black tracking-wide text-center px-3 py-2 uppercase duration-300 transition-all 
-						${ currentType === type ? 'bg-white text-blue-800 border border-black' : ''}`}
+						${ currentCount === type ? 'bg-white text-blue-800 border border-black' : ''}`}
 				>
-					{type}
+					{type.name}
 				</div>
 			))}
 		</div>
